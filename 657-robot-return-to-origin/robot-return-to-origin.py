9f8c1b3b238945128fd2d4f71c8d5c0be4ed8864
paulatul9015 <atulpalstudy12@gmail.com>
2026-04-05 13:39:05 +0530

    Time: 1 ms (88.41%) | Memory: 12.7 MB (34.64%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.py
+++ 657-robot-return-to-origin/robot-return-to-origin.py
@@ -1,22 +1,5 @@
 class Solution(object):
     def judgeCircle(self, moves):
-        """
-        :type moves: str
-        :rtype: bool
-        """
-        # Initialize coordinates
-        x, y = 0, 0
-        
-        for move in moves:
-            if move == 'U':
-                y += 1
-            elif move == 'D':
-                y -= 1
-            elif move == 'L':
-                x -= 1
-            elif move == 'R':
-                x += 1
-        
-        # Check if we are back at (0, 0)
-        return x == 0 and y == 0
-        
+        # A robot returns home if horizontal moves cancel out 
+        # and vertical moves cancel out.
+        return moves.count('U') == moves.count('D') and moves.count('L') == moves.count('R')
