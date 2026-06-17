301893a67b5792102dd751982298b1be2e50f4d8
paulatul9015 <atulpalstudy12@gmail.com>
2026-06-18 02:29:39 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (20.24%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -6,9 +6,13 @@ class Solution(object):
         """
         if n <= 2 :
             return n 
-        dp = [0] * (n+1 )
-        dp[1] = 1 
-        dp[2] = 2 
-        for i in range(3,n+1):
-            dp[i] = dp[i-1] + dp[i-2]
-        return dp[n]
+        prev2 = 1
+        prev1 = 2
+
+        for i in range(3, n + 1):
+
+            curr = prev1 + prev2
+
+            prev2 = prev1
+            prev1 = curr
+        return prev1
