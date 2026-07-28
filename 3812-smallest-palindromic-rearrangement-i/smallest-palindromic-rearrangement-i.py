48f8894f1d94768dd9d23aece22561deac2b0551
paulatul9015 <atulpalstudy12@gmail.com>
2026-07-28 12:47:28 +0530

    Time: 752 ms (11.76%) | Memory: 13.8 MB (44.12%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def smallestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        freq = Counter(s)
+
+        left = []
+        middle = ""
+
+        for ch in sorted(freq):
+            left.append(ch * (freq[ch] // 2))
+            if freq[ch] % 2:
+                middle = ch
+
+        left = "".join(left)
+        return left + middle + left[::-1]        
